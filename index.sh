ae1812f9a516163e204e1a2f9a610d911e4890de
JoaoYamaguti <joaov.yamaguti@gmail.com>
2023-07-21 19:12:42 -0300

    New save version

--- index.sh
+++ index.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-ds2Path="/media/HD02/SteamLibrary/steamapps/compatdata/335300/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsII/"
-ds3Path="/media/HD02/SteamLibrary/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII/"
+ds2Path="C:\Users\João\AppData\Roaming\DarkSoulsII"
+# ds3Path="/media/HD02/SteamLibrary/steamapps/compatdata/374320/pfx/drive_c/users/steamuser/AppData/Roaming/DarkSoulsIII/"
 
 cp -R "$ds2Path"/* ./DarkSoulsII
-cp -R "$ds3Path"/* ./DarkSoulsIII
+# cp -R "$ds3Path"/* ./DarkSoulsIII
 
 echo "Save transfered successfully"
